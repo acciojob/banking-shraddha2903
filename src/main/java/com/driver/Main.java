b8cf8711ca0f99601290a7e79cce9b937be7c01f
shraddha2903 <70905602+shraddha2903@users.noreply.github.com>
2023-05-28 16:04:30 +0530

    commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,10 +3,6 @@ package com.driver;
 public class Main {
     public static void main(String[] args) throws InValidLicenseIdException {
 
-       CurrentAccount currentAccount = new CurrentAccount("curr", 10000.0, "ABC");
-        currentAccount.validateLicenseId();
-        System.out.println(currentAccount.getTradeLicenseId());
-
 
     }
 }
